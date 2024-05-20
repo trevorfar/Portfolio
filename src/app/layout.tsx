16674a0d96076c06e7a009c3d5b0ac93ebf70910
Trevor Farias <levelonethousandr@gmail.com>
2024-05-20 12:20:51 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,33 @@
-import type { Metadata } from "next";
-import { Gabarito } from "next/font/google";
-import "./globals.css";
-import { Suspense } from "react";
-import Navbar from "@/Components/Navbar";
+import type { Metadata } from "next"
+import { Gabarito } from "next/font/google"
+import "./globals.css"
+import { Suspense } from "react"
+import Navbar from "@/Components/Navbar"
 
-const gabarito = Gabarito({ subsets: ["latin"] });
+const gabarito = Gabarito({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Trevor Farias - Portfolio",
   description: "Trevor Farias's Portfolio",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      
       <head>
         <link rel="shortcut icon" href="./favicon.ico" />
       </head>
       <body className={gabarito.className}>
-            {/* ADD NAVBAR */}
-            <Navbar />
+        {/* ADD NAVBAR */}
+        <Navbar />
 
-            <div className="z-20">{children}</div>
-            <div className="fixed top-0 w-full z-[-1] opacity-50 dark:opacity-30">
-            </div>
-            {/* Analytics and SpeedInsights components*/}
+        <div className="z-20">{children}</div>
+        <div className="fixed top-0 w-full z-[-1] opacity-50 dark:opacity-30"></div>
+        {/* Analytics and SpeedInsights components*/}
       </body>
     </html>
   )
